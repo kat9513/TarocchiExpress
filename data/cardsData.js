08b767a0139cc78d173763ef2182b6d60a6f5dae
Katie Higgins <katherine9513@gmail.com>
2019-05-13 15:34:18 -0500

    adding cards data

---
+++ data/cardsData.js
@@ -0,0 +1,134 @@
+var cardsArray =     [
+    {
+        "id": 1,
+        "name": "The Fool",
+        "description":"",
+        "img":""
+    },
+    {
+        "id": 2,
+        "name": "The Magician",
+        "description":"",
+        "img":""
+    },
+    {
+        "id": 3,
+        "name": "The High Priestess",
+        "description":"",
+        "img":""
+    },
+    {
+        "id": 4,
+        "name": "The Empress",
+        "description":"",
+        "img":""
+    },
+    {
+        "id": 5,
+        "name": "The Emperor",
+        "description":"",
+        "img":""
+    },
+    {
+        "id": 6,
+        "name": "The Hierophant",
+        "description":"",
+        "img":""
+    },
+    {
+        "id": 7,
+        "name": "The Lovers",
+        "description":"",
+        "img":""
+    },
+    {
+        "id": 8,
+        "name": "The Chariot",
+        "description":"",
+        "img":""
+    },
+    {
+        "id": 9,
+        "name": "Strength",
+        "description":"",
+        "img":""
+    },
+    {
+        "id": 10,
+        "name": "The Hermit",
+        "description":"",
+        "img":""
+    },
+    {
+        "id": 11,
+        "name": "Wheel of Fortune",
+        "description":"",
+        "img":""
+    },
+    {
+        "id": 12,
+        "name": "Justice",
+        "description":"",
+        "img":""
+    },
+    {
+        "id": 13,
+        "name": "The Hanged Man",
+        "description":"",
+        "img":""
+    },
+    {
+        "id": 14,
+        "name": "Death",
+        "description":"",
+        "img":""
+    },
+    {
+        "id": 15,
+        "name": "Temperance",
+        "description":"",
+        "img":""
+    },
+    {
+        "id": 16,
+        "name": "The Devil",
+        "description":"",
+        "img":""
+    },
+    {
+        "id": 17,
+        "name": "The Tower",
+        "description":"",
+        "img":""
+    },
+    {
+        "id": 18,
+        "name": "The Star",
+        "description":"",
+        "img":""
+    },
+    {
+        "id": 19,
+        "name": "The Moon",
+        "description":"",
+        "img":""
+    },
+    {
+        "id": 20,
+        "name": "The Sun",
+        "description":"",
+        "img":""
+    },
+    {
+        "id": 21,
+        "name": "Judgement",
+        "description":"",
+        "img":""
+    },
+    {
+        "id": 22,
+        "name": "The World",
+        "description":"",
+        "img":""
+    }
+]
