d01c56e9e792d4a50fb092e76d14c613e7860298
Katie Higgins <katherine9513@gmail.com>
2019-05-21 13:35:21 -0500

    serving and then connecting css to html

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 //dependencies
 var express = require("express");
 var path = require("path");
-
 //set up express app
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static(path.join(__dirname, '/app/public')));
+
+/*app.get("/css", function (req, res){
+    res.sendFile(__dirname + "/app/public/assets/style.css")
+});*/
+
+console.log(path.resolve(__dirname, './'))
+
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 //routes
