bf0547e860af1da0789841dbeba5926b23e358d8
Katie Higgins <katherine9513@gmail.com>
2019-05-22 16:35:06 -0500

    reconfiguring cardsData json array, adding descriptions HOORAY

--- app/data/cardsData.js
+++ app/data/cardsData.js
@@ -4,311 +4,290 @@ var cardsArray = [
     {
         "id": 1,
         "name": "The Fool",
-        "description": 
-        [
-            "upright",
-            "reversed"
-        ],
-        
-        "img": 
-        [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theFool_zpsbl6v2sve.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theFoolReverse_zpsjiaqx3l4.jpg"
-        ]
+        "img":
+            [
+                {
+                    "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theFool_zpsbl6v2sve.jpg", "description": "upright"
+                },
+                {
+                    "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theFoolReverse_zpsjiaqx3l4.jpg", "description": "reversed"
+                }
+
+            ]
 
 
     },
     {
         "id": 2,
         "name": "The Magician",
-        "description": 
-        [
-            "upright",
-            "reversed"
-        ],
-        "img": 
-        [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theMagician_zps8zwbpigd.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theMagicianReverse_zpsmef3zloi.jpeg"
-        ]
+        "img":
+            [
+                {
+                    "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theMagician_zps8zwbpigd.jpg", "description": "upright"
+                },
+                {
+                    "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theMagicianReverse_zpsmef3zloi.jpeg", "description": "reversed"
+                }
+            ]
     },
     {
         "id": 3,
         "name": "The High Priestess",
-        "description": 
-        [
-            "upright",
-            "reversed"
-        ],
-        "img": 
-        [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/highPriestess_zpsf47rtdkf.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/highPriestessReverse_zpsbdf1lbio.jpeg"
-        ]
+        "img":
+            [
+                {
+                    "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/highPriestess_zpsf47rtdkf.jpg", "description": "upright"
+                },
+                {
+                    "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/highPriestessReverse_zpsbdf1lbio.jpeg", "description": "reversed"
+                }
+            ]
     },
     {
         "id": 4,
         "name": "The Empress",
-        "description": 
-        [
-            "upright",
-            "reversed"
-        ],
-        "img": 
-        [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theEmpress_zpstyrugqlh.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theEmpressReverse_zpsfk7o9apx.jpeg"
-        ]
+        "img":
+            [
+                {
+                    "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theEmpress_zpstyrugqlh.jpg", "description": "upright"
+                },
+                {
+                    "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theEmpressReverse_zpsfk7o9apx.jpeg", "description": "reversed"
+                }
+            ]
     },
     {
         "id": 5,
         "name": "The Emperor",
-        "description": 
-        [
-            "upright",
-            "reversed"
-        ],
-        "img": 
-        [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theEmporer_zpsviolpx1c.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theEmporerReverse_zpsj5rvfpjp.jpeg"
-        ]
+        "img":
+            [
+                {
+                    "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theEmporer_zpsviolpx1c.jpg", "description": "upright"
+                },
+                {
+                    "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theEmporerReverse_zpsj5rvfpjp.jpeg", "description": "reversed"
+                }
+            ]
     },
     {
         "id": 6,
         "name": "The Hierophant",
-        "description": 
-        [
-            "upright",
-            "reversed"
-        ],
-        "img": 
-        [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theHierophant_zpswq9pe5ga.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theHierophantReverse_zpsqjkk7mdj.jpeg"
-        ]
+        "img":
+            [
+                {
+                    "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theHierophant_zpswq9pe5ga.jpg", "description": "upright"
+                },
+                {
+                    "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theHierophantReverse_zpsqjkk7mdj.jpeg", "description": "reversed"
+                }
+            ]
     },
     {
         "id": 7,
         "name": "The Lovers",
-        "description": 
-        [
-            "upright",
-            "reversed"
-        ],
-        "img": 
-        [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theLovers_zpsrddpqlcn.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theLoversReverse_zpsbzbir2dn.jpeg"
-        ]
+        "img":
+            [
+                {
+                    "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theLovers_zpsrddpqlcn.jpg", "description": "upright"
+                },
+                {
+                    "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theLoversReverse_zpsbzbir2dn.jpeg", "description": "reversed"
+                }
+            ]
     },
     {
         "id": 8,
         "name": "The Chariot",
-        "description": 
-        [
-            "upright",
-            "reversed"
-        ],
-        "img": 
-        [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theChariot_zps1tycc6hs.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theChariotReverse_zps2jvdgtzg.jpeg"
-        ]
+        "img":
+            [
+                {
+                    "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theChariot_zps1tycc6hs.jpg", "description": "upright"
+                },
+                {
+                    "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theChariotReverse_zps2jvdgtzg.jpeg", "description": "reversed"
+                }
+            ]
     },
     {
         "id": 9,
         "name": "Strength",
-        "description":         
-        [
-            "upright",
-            "reversed"
-        ],
-        "img": 
-        [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/Strength_zpsge3xtzvj.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/StrengthReverse_zpstptpevk4.jpeg"
-        ]
+        "img":
+            [
+                {
+                    "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/Strength_zpsge3xtzvj.jpg", "description": "upright"
+                },
+                {
+                    "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/StrengthReverse_zpstptpevk4.jpeg", "description": "reversed"
+                }
+            ]
     },
     {
         "id": 10,
         "name": "The Hermit",
-        "description": 
-        [
-            "upright",
-            "reversed"
-        ],
-        "img": 
-        [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theHermit_zpshxbw7bm9.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theHermitReverse_zpsxxixfdvl.jpeg"
-        ]
+        "img":
+            [
+                {
+                    "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theHermit_zpshxbw7bm9.jpg", "description": "upright"
+                },
+                {
+                    "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/theHermitReverse_zpsxxixfdvl.jpeg", "description": "reversed"
+                }
+            ]
     },
     {
         "id": 11,
         "name": "Wheel of Fortune",
-        "description": 
-        [
-            "upright",
-            "reversed"
-        ],
         "img": 
         [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/wheelOfFortune_zpswhmpyala.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/wheelOfFortuneReverse_zpsucptgisv.jpeg"
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/wheelOfFortune_zpswhmpyala.jpg", "description": "upright"
+            },
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/wheelOfFortuneReverse_zpsucptgisv.jpeg", "description": "reversed"
+            }
         ]
     },
     {
         "id": 12,
         "name": "Justice",
-        "description": 
+        "img": 
         [
-            "upright",
-            "reversed"
-        ],
-        "img": [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/Justice_zpsaxgnzjbz.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/JusticeReverse_zps7fypyqve.jpeg"
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/Justice_zpsaxgnzjbz.jpg","description": "upright"
+            },
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/JusticeReverse_zps7fypyqve.jpeg", "description": "reversed"
+            }
         ]
     },
     {
         "id": 13,
         "name": "The Hanged Man",
-        "description": 
-        [
-            "upright",
-            "reversed"
-        ],
         "img": 
         [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/hangedMan_zpss04thidy.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/hangedManReverse_zpscdqjdfa8.jpeg"
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/hangedMan_zpss04thidy.jpg","description": "upright"
+            },
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/hangedManReverse_zpscdqjdfa8.jpeg", "description": "reversed"
+            }
         ]
     },
     {
         "id": 14,
         "name": "Death",
-        "description": 
-        [
-            "upright",
-            "reversed"
-        ],
         "img": 
         [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/Death_zpsyt972v0w.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/DeathReverse_zpsthknfmg8.jpeg"
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/Death_zpsyt972v0w.jpg","description": "upright"
+            },
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/DeathReverse_zpsthknfmg8.jpeg", "description": "reversed"
+            }
         ]
     },
     {
         "id": 15,
         "name": "Temperance",
-        "description": 
-        [
-            "upright",
-            "reversed"
-        ],
         "img": 
         [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/Temperance_zpsyioi4vwm.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/TemperanceReverse_zps7y77ek2l.jpeg"
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/Temperance_zpsyioi4vwm.jpg","description": "upright"
+            },
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/TemperanceReverse_zps7y77ek2l.jpeg", "description": "reversed"
+            }
         ]
     },
     {
         "id": 16,
         "name": "The Devil",
-        "description": 
-        [
-            "upright",
-            "reversed"
-        ],
         "img": 
         [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/Devil_zpslelc1dmr.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/DevilReverse_zpszh0ywykp.jpeg"
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/Devil_zpslelc1dmr.jpg","description": "upright"
+            },
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/DevilReverse_zpszh0ywykp.jpeg", "description": "reversed"
+            }
         ]
     },
     {
         "id": 17,
         "name": "The Tower",
-        "description": 
-        [
-            "upright",
-            "reversed"
-        ],
         "img": 
         [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/Tower_zpsa5jo33la.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/TowerReverse_zpsf46yhzxr.jpeg"
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/Tower_zpsa5jo33la.jpg","description": "upright"
+            },
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/TowerReverse_zpsf46yhzxr.jpeg", "description": "reversed"
+            }
         ]
     },
     {
         "id": 18,
         "name": "The Star",
-        "description": 
-        [
-            "upright",
-            "reversed"
-        ],
         "img": 
         [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/Star_zpsfxn7xac5.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/StarReverse_zpsjwi3kp28.jpeg"
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/Star_zpsfxn7xac5.jpg","description": "upright"
+            },
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/StarReverse_zpsjwi3kp28.jpeg", "description": "reversed"
+            }
         ]
     },
     {
         "id": 19,
         "name": "The Moon",
-        "description": 
-        [
-            "upright",
-            "reversed"
-        ],
         "img": 
         [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/Moon_zpstkrirzez.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/MoonReverse_zpspyvotbkz.jpeg"
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/Moon_zpstkrirzez.jpg","description": "upright"
+            },
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/MoonReverse_zpspyvotbkz.jpeg", "description": "reversed"
+            }
         ]
     },
     {
         "id": 20,
         "name": "The Sun",
-        "description": 
-        [
-            "upright",
-            "reversed"
-        ],
         "img": 
         [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/Sun_zpss1tve2tm.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/SunReverse_zpstkzw4kax.jpeg"
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/Sun_zpss1tve2tm.jpg","description": "upright"
+            },
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/SunReverse_zpstkzw4kax.jpeg", "description": "reversed"
+            }
         ]
     },
     {
         "id": 21,
         "name": "Judgement",
-        "description": 
-        [
-            "upright",
-            "reversed"
-        ],
         "img": 
         [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/Judgement_zps3jhe677i.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/JudgementReverse_zpsxvnftzd4.jpeg"
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/Judgement_zps3jhe677i.jpg","description": "upright"
+            },
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/JudgementReverse_zpsxvnftzd4.jpeg", "description": "reversed"
+            }
         ]
     },
     {
         "id": 22,
         "name": "The World",
-        "description": 
-        [
-            "upright",
-            "reversed"
-        ],
         "img": 
         [
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/World_zpseb9wykds.jpg",
-            "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/World_zpseb9wykds.jpg"
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/World_zpseb9wykds.jpg","description": "upright"
+            },
+            {
+                "image": "https://i48.photobucket.com/albums/f209/Katie_Higgins/tarocchi/WorldReverse_zpsgxib1a1p.jpeg", "description": "reversed"
+            }
         ]
     }
 ];
